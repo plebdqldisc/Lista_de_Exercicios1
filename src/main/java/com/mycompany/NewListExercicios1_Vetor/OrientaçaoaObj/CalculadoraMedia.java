abac20d60b1db0b562413ac205a453514df16ce5
plebdqldisc <129127413+plebdqldisc@users.noreply.github.com>
2023-09-14 13:50:41 -0300

    Update CalculadoraMedia.java

--- src/main/java/com/mycompany/NewListExercicios1_Vetor/OrientaçaoaObj/CalculadoraMedia.java
+++ src/main/java/com/mycompany/NewListExercicios1_Vetor/OrientaçaoaObj/CalculadoraMedia.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package OrientaçãoaObj;
+package com.mycompany.NewListExercicios1_Vetor.OrientaçaoaObj;
 
 /**
  *
- * @author Usuario
+ * @author arthur.7923
  */
 public class CalculadoraMedia {
     // Classe base para a Calculadora
