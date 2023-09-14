bdd0c19ad9a69abe064dad7035046a848c17ca01
plebdqldisc <129127413+plebdqldisc@users.noreply.github.com>
2023-09-14 02:53:37 -0300

    Update CalculadoraMedia.java

--- src/main/java/com/mycompany/NewListExercicios1_Vetor/OrientaçaoaObj/CalculadoraMedia.java
+++ src/main/java/com/mycompany/NewListExercicios1_Vetor/OrientaçaoaObj/CalculadoraMedia.java
@@ -2,27 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.NewListExercicios1_Vetor.OrientaçaoaObj;
+package OrientaçãoaObj;
 
 /**
  *
- * @author arthur.7923
+ * @author Usuario
  */
-public class CalculadoraMedia{
+public class CalculadoraMedia {
     // Classe base para a Calculadora
-    
-    
-    float MeArSi = mediaSimples(0, 0);
-    float MeArPo = mediaPonderada(0, 0, 0, 0);
 
     // Método para calcular a média simples
-    public float mediaSimples(float a, float b) {
-        return (a + b) / 2;
+    public float mediaSimples(float digit1, float digit2) {
+        return (digit1 + digit2) / 2;
     }
 
-    // Método para calcular a média ponderada ("a e b" são nums [a = num1, b = num2]. Os "A e B" são os pesos [ps1 = peso1, B = peso2]
-    public float mediaPonderada(float a, float ps1, float b, float ps2) {
-        return (a * ps1 + b * ps2) / (ps1 + ps2);
+    // Método para calcular a média ponderada (Os digit sao nums: digit1 = num1, digit2 = num2)
+    public float mediaPonderada(float digit1, float peso1, float digit2, float peso2) {
+        return (digit1 * peso1 + digit2 * peso2) / (peso1 + peso2);
     }
 }
-
